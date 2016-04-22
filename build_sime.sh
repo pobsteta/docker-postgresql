57d73fbae18292cc1bf8c335df6f8f61fddf5478
Pascal Obstétar <pascal.obstetar@bioecoforests.com>
2016-04-22 19:20:45 +0200

    maj build_sime.sh

--- build_sime.sh
+++ build_sime.sh
@@ -9,6 +9,6 @@ docker rmi $(docker images -q)
 # Force la suppression des images de base
 #docker rmi -f $(docker images -q)
 # Construit l'image
-docker build -t pobsteta/docker-postgresql:1.0 .
+docker build -t pobsteta/docker-postgresql .
 # Lance le container créé détaché (argument -d)
-#docker run -p 35432:5432 -p 38000:8000 pobsteta/docker-postgresql:1.0 /sbin/my_init
+#docker run -p 35432:5432 -p 38000:8000 pobsteta/docker-postgresql
